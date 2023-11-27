71ccacd6cc940a28e8ad080178e9ee6c7ac59c01
Jakub Szewc <jakub.szewc62@gmail.com>
2023-11-27 23:46:41 +0100

    Having fun with css I have to work on naming commits ngl

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-const observer = new IntersectionObserver((visibleToUser) => {
-    visibleToUser.forEach((isVisible) => {
-        console.log(isVisible)
-        if (isVisible.isIntersecting) {
-            isVisible.target.classList.add('show')
-        } else {
-            isVisible.target.classList.remove('show')
-        }
+{
+  const observer = new IntersectionObserver(visibleToUser => {
+    visibleToUser.forEach(isVisible => {
+      console.log(isVisible)
+      if (isVisible.isIntersecting) {
+        isVisible.target.classList.add('show')
+      } else {
+        isVisible.target.classList.remove('show')
+      }
     })
-})
+  })
 
-const hiddenElements = document.querySelectorAll('.hidden')
-hiddenElements.forEach((element) => observer.observe(element))
+  const hiddenElements = document.querySelectorAll('.hidden')
+  hiddenElements.forEach(element => observer.observe(element))
+
+  const icon = document.querySelector('.icon')
+  const navItems = document.querySelectorAll('.notVisible')
+  icon.onclick = navBarShow
+  function navBarShow () {
+    navItems.forEach(item => {
+        item.classList.toggle('visible')
+    })
+  }
+}
